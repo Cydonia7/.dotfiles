7e38fe8f2e82042ce7fa321a44714465ab2ef2c2
Thomas Royer <cydonia572@gmail.com>
2018-12-27 10:24:37 +0100

    Network manager applet

--- config/polybar/launch_polybar.sh
+++ config/polybar/launch_polybar.sh
@@ -1,5 +1,6 @@
 killall polybar
-for m in $(polybar --list-monitors | cut -d":" -f1); do
+# Sed is used to get them in reverse order
+for m in $(polybar --list-monitors | cut -d":" -f1 | sed '1!G;h;$!d'); do
     MONITOR=$m polybar --reload example &
 done
 
