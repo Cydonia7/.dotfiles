7ae123b29017805a5f6f375ee7123c895c5c9e40
Thomas Royer <Cydonia7@users.noreply.github.com>
2022-03-17 13:10:26 +0100

    Update cycle_workspace.py

--- config/i3/cycle_workspace.py
+++ config/i3/cycle_workspace.py
@@ -11,11 +11,11 @@ outputs = i3.get_outputs()
 workspaces = i3.get_workspaces()
 
 # figure out what is on, and what is currently on your screen.
-workspace = list(filter(lambda s: s['focused']==True, workspaces))
-output = list(filter(lambda s: s['active']==True, outputs))
+workspace = list(filter(lambda s: s.focused, workspaces))
+output = list(filter(lambda s: s.active, outputs))
 
-# figure out the other workspace name
-other_workspace = list(filter(lambda s: s['name']!=workspace[0]['output'], output))
+# figure out the other output name
+other_output = list(filter(lambda s: s.name!=workspace[0].output, output))
 
 # send current to the no-active one
-i3.command('move workspace to output '+other_workspace[0]['name'])
+i3.command('move workspace to output '+other_output[0].name)
